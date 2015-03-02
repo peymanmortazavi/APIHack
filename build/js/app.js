062a888078f2d8e51ac9cb9d862229a788f8de53
Nir Boneh <nboneh5921@gmail.com>
2015-03-02 16:48:42 -0700

    Framework

---
+++ build/js/app.js
@@ -0,0 +1,12 @@
+$( "#tweets" ).on( "click", function( event ) {
+    tweets.load()
+})
+$( "#trends" ).on( "click", function( event ) {
+    trends.load()
+})
+$( "#users" ).on( "click", function( event ) {
+    users.load()
+})
+$( "#locations" ).on( "click", function( event ) {
+    locations.load()
+})
